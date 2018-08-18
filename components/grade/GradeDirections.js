eec61f52488f362f2a05afbe60d680ca65c0b3c4
Sean McPherson <seanmcp25@gmail.com>
2018-08-18 07:49:43 -0400

    Create GradeDirections component

---
+++ components/grade/GradeDirections.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default GradeDirections = () => (
+    <View style={styles.container}>
+        <Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde consequuntur minus error doloremque placeat, neque fuga ducimus eius aliquid aliquam, id libero nemo perferendis incidunt quia molestiae ad deleniti culpa.
+        </Text>
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {}
+})
