411925a435dc503d64a2848b520bbe5752fa92a6
Sean McPherson <seanmcp25@gmail.com>
2018-08-23 06:57:51 -0400

    Restyle ViewHeaderLayout
    Removed Heading component to provide more styling flexibility. Find a
    way to pass backgroundColor prop based on button color.

--- components/layout/ViewHeaderLayout.js
+++ components/layout/ViewHeaderLayout.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import Heading from '../text/Heading';
 import Icon from '../atomic/Icon';
-import { COLOR } from '../../constants';
+import COLOR from '../../constants/Color';
 
 export default ViewHeaderLayout = (props) => (
     <View style={styles.container}>
-        <View style={styles.header}>
+        <View style={[ styles.header, { backgroundColor: props.backgroundColor || COLOR.gray500 } ]}>
             <Icon
                 color="white"
                 name="md-arrow-back"
                 onPress={props.handleBack}
             />
-            <Heading>
+            <Text style={styles.heading}>
                 {props.title}
-            </Heading>
+            </Text>
         </View>
         {props.children}
     </View>
@@ -23,11 +23,22 @@ export default ViewHeaderLayout = (props) => (
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
-        width: '100vw'
+        flex: 1,
+        width: '100%',
     },
     header: {
-        alignSelf: 'stretch',
-        backgroundColor: COLOR.blue800,
+        alignItems: 'center',
+        backgroundColor: COLOR.blue500,
         display: 'flex',
+        flexDirection: 'row',
+        padding: 10,
+        width: '100%',
+    },
+    heading: {
+        color: 'white',
+        fontSize: 20,
+        flex: 1,
+        marginRight: 30,
+        textAlign: 'center',
     },
 })
