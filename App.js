162f63542ac0bc0442aa5bb66ff1e5ee7cd01268
Sean McPherson <seanmcp25@gmail.com>
2018-08-23 06:52:08 -0400

    Make components full height
    Required { flex: 1 } style property.

--- App.js
+++ App.js
@@ -45,9 +45,9 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#fff',
         alignItems: 'center',
+        backgroundColor: '#fff',
+        flex: 1,
         justifyContent: 'center',
     },
 });
--- views/GradeView.js
+++ views/GradeView.js
@@ -60,5 +60,8 @@ export default class GradeView extends Component {
 const styles = StyleSheet.create({
     container: {
         display: 'flex',
+        flex: 1,
+        padding: 10,
+        width: '100%',
     },
 });
