4545054e1e33ba483d0e32d883d4a785f4615b0d
Sean McPherson <seanmcp25@gmail.com>
2018-08-24 07:09:01 -0400

    Add conditional label and help text fields

--- components/atomic/Input.js
+++ components/atomic/Input.js
@@ -1,15 +1,29 @@
 import React from 'react';
-import { StyleSheet, TextInput } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default Input = (props) => (
-    <TextInput
-        style={styles.input}
-        {...props}
-    />
+export default Input = ({ help, label, style, ...rest }) => (
+    <View style={style}>
+        {label ? (
+            <Text style={styles.label}>{label}</Text>
+        ) : null}
+        <TextInput
+            style={styles.input}
+            {...rest}
+        />
+        {help ? (
+            <Text style={styles.help}>{help}</Text>
+        ) : null}
+    </View>
 )
 
 const styles = StyleSheet.create({
+    help: {
+        fontSize: 12,
+    },
     input: {
-        padding: 10,
+        padding: 8,
+    },
+    label: {
+        fontWeight: 'bold'
     }
 })
