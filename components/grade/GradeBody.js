e2b1f504c0beb9739af33ef097c2927dfe4351b7
Sean McPherson <seanmcp25@gmail.com>
2018-08-24 07:10:06 -0400

    Use ScrollView instead of View

--- components/grade/GradeBody.js
+++ components/grade/GradeBody.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import GradeDirections from './GradeDirections';
 import GradeTable from './GradeTable';
 
 export default GradeBody = (props) => (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
         {props.options.length === 0 ? (
             <GradeDirections />
         ) : (
             <GradeTable options={props.options} places={props.places} />
         )}
-    </View>
+    </ScrollView>
 )
 
 const styles = StyleSheet.create({
-    container: {}
+    container: {
+        marginTop: 16,
+    },
 })
