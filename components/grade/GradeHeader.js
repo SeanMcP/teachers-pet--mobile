1cdb3debc404d33ce56e244b9f24d0e7419337bd
Sean McPherson <seanmcp25@gmail.com>
2018-08-24 07:09:52 -0400

    Change style and positioning
    Side-by-side inputs conserves vertical space.

--- components/grade/GradeHeader.js
+++ components/grade/GradeHeader.js
@@ -9,20 +9,25 @@ import Input from '../atomic/Input';
 
 export default GradeHeader = (props) => (
     <View style={styles.container}>
-        <Input
-            keyboardType="numeric"
-            onChangeText={(text) => props.handleChange('problems', text)}
-            onSubmitEditing={Keyboard.dismiss}
-            placeholder="Number of problems"
-            value={props.problemsValue}
-        />
-        <Input
-            keyboardType="numeric"
-            onChangeText={(text) => props.handleChange('decimalPlaces', text)}
-            onSubmitEditing={Keyboard.dismiss}
-            placeholder="Number of decimal places"
-            value={props.placesValue}
-        />
+        <View style={styles.inputContainer}>
+            <Input
+                keyboardType="numeric"
+                label="Problems"
+                onChangeText={(text) => props.handleChange('problems', text)}
+                onSubmitEditing={Keyboard.dismiss}
+                style={[ styles.input, styles.inputLeft ]}
+                value={props.problemsValue}
+            />
+            <Input
+                keyboardType="numeric"
+                label="Decimal places"
+                onChangeText={(text) => props.handleChange('decimalPlaces', text)}
+                onSubmitEditing={Keyboard.dismiss}
+                placeholder="Default: 0"
+                style={[ styles.input, styles.inputRight ]}
+                value={props.placesValue}
+            />
+        </View>
         <Button
             disabled={!props.problemsValue}
             onPress={props.handleClick}
@@ -33,6 +38,20 @@ export default GradeHeader = (props) => (
 
 const styles = StyleSheet.create({
     container: {
-        display: 'flex'
+        display: 'flex',
+    },
+    input: {
+        flex: 1,
+        marginBottom: 8,
+    },
+    inputContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    inputLeft: {
+        marginRight: 8
+    },
+    inputRight: {
+        marginLeft: 8
     }
 })
