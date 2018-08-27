e25ae2caba96329656d5c9d21d464f248212f367
Sean McPherson <seanmcp25@gmail.com>
2018-08-27 06:58:36 -0400

    Add perGroup renderGroups condition

--- components/group/GroupBody.js
+++ components/group/GroupBody.js
@@ -11,18 +11,28 @@ const renderGroups = ({ groupsValue, perGroupValue, totalValue }) => {
     const groups = Number(groupsValue),
     perGroup = Number(perGroupValue),
     total = Number(totalValue);
+
+    let count, num, rem;
     
     if (groups) {
-        const count = Math.floor(total / groups);
-        const rem = total % groups;
-
-        return (
-            <View>
-                <Text>Count off by {count}'s</Text>
-                {rem ? <Text>({rem} left over)</Text> : null}
-            </View>
-        );
+        count = Math.floor(total / groups);
+        num = groups;
+        rem = total % groups;
     }
+    
+    if (perGroup) {
+        count = perGroup;
+        num = Math.floor(total / perGroup);
+        rem = total % perGroup;
+    }
+
+    return (
+        <View>
+            <Text>Count off by {count}'s</Text>
+            <Text>Makes {num} groups of {count}</Text>
+            {rem ? <Text>({rem} left over)</Text> : null}
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
